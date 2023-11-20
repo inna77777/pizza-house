5192ed5cde3d10f1e18b58c600dbde40b58d026b
Inna <inna42320@gmail.com>
2023-11-20 17:33:29 +0100

    added

--- src/homePage/GridImagesSection.js
+++ src/homePage/GridImagesSection.js
@@ -14,14 +14,14 @@ const GridImagesSection = () => {
       <div className="grid-container">
         <div className="hover-child">
           <img src={ingredients} alt="" />
-          <div className="text">
+          <div className="text2">
             <p>BEST INGREDIENTS</p>
             <span>TASTY PIZZA</span>
           </div>
         </div>
         <div className="hover-child">
           <img src={interior} alt="" />
-          <div className="text">
+          <div className="text2">
             <p>COMFORTABLE INTERIOR</p>
             <span>MODERN DESIGN</span>
           </div>
@@ -29,35 +29,35 @@ const GridImagesSection = () => {
         <div className="inside-grid-container">
           <div className="dishware">
             <img src={dishware} alt="" />
-            <div className="text">
+            <div className="text2">
               <p>QUALITY DISHWARE</p>
               <span>TOP-NOTCH UTENSILS</span>
             </div>
           </div>
           <div className="cocktails">
             <img src={cocktails} alt="" />
-            <div className="text">
+            <div className="text2">
               <p>REFRESHING COCKTAILS</p>
               <span>EXCLUSIVE SELECTION</span>
             </div>
           </div>
           <div className="salads">
             <img src={salads} alt="" />
-            <div className="text">
+            <div className="text2">
               <p>EXOTIC SALADS</p>
               <span>SUMMER TASTE</span>
             </div>
           </div>
           <div className="pizzasTypes">
             <img src={pizzasTypes} alt="" />
-            <div className="text">
+            <div className="text2">
               <p>ALL TYPES OF PIZZA</p>
               <span>SPECIAL RECIPES</span>
             </div>
           </div>
           <div className="diverseMenu">
             <img src={diverseMenu} alt="" />
-            <div className="text">
+            <div className="text2">
               <p>DIVERSE MENU</p>
               <span>PICK YOUR FAVORITE DISH</span>
             </div>
